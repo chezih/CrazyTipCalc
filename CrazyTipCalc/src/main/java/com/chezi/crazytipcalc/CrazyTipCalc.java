af44e4b0efe5f39c98fd49af920443944c386fb5
chezih <chezih@gmail.com>
2014-06-06 18:43:29 +0300

    Added the logic of the app

--- CrazyTipCalc/src/main/java/com/chezi/crazytipcalc/CrazyTipCalc.java
+++ CrazyTipCalc/src/main/java/com/chezi/crazytipcalc/CrazyTipCalc.java
@@ -2,18 +2,90 @@ package com.chezi.crazytipcalc;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 
 public class CrazyTipCalc extends ActionBarActivity {
+
+    private static final String TOTAL_BILL = "TOTAL_BILL";
+    private static final String CURRENT_TIP = "CURRENT_TIP";
+    private static final String BILL_WITHOUT_TIP = "BILL_WITHOUT_TIP ";
+
+    private double billBeforeTip;
+    private double tipAmount;
+    private double finalBill;
+
+    EditText billBeforeTipET;
+    EditText tipAmountET;
+    EditText finalBillET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crazy_tip_calc);
+
+        if (savedInstanceState == null) {
+            billBeforeTip = 0.0;
+            tipAmount = .15;
+            finalBill = 0.0;
+        } else {
+            billBeforeTip = savedInstanceState.getDouble(BILL_WITHOUT_TIP);
+            tipAmount = savedInstanceState.getDouble(CURRENT_TIP);
+            finalBill = savedInstanceState.getDouble(TOTAL_BILL);
+        }
+
+        billBeforeTipET = (EditText) findViewById(R.id.billEditText);
+        tipAmountET = (EditText) findViewById(R.id.tipEditText);
+        finalBillET = (EditText) findViewById(R.id.finalBillEditText);
+
+        billBeforeTipET.addTextChangedListener(billBeforeTipListener);
     }
 
+    private TextWatcher billBeforeTipListener = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+                try {
+                    billBeforeTip = Double.parseDouble(charSequence.toString());
+                }catch (NumberFormatException e){
+                    billBeforeTip=0.0;
+                }
+
+            updateTipAndFinalBill();
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
+    };
+
+
+    private void updateTipAndFinalBill()
+    {
+        double tipAmount = Double.parseDouble(tipAmountET.getText().toString());
+
+        double finalBill= billBeforeTip+(tipAmount*billBeforeTip);
+
+        finalBillET.setText(String.format("%.02f",finalBill));
+    }
+
+    protected  void  onSaveInstanceState(Bundle outState)
+    {
+        super.onSaveInstanceState(outState);
+
+        outState.putDouble(TOTAL_BILL,finalBill);
+        outState.putDouble(CURRENT_TIP,tipAmount);
+        outState.putDouble(BILL_WITHOUT_TIP,billBeforeTip);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
